a35277c245a26822c78d3ec82799e887cf97606c
Kevin Wolf <hi@kevinwolf.cr>
2023-08-31 12:32:55 -0600

    refactor: simplify middleware since vercel preview environments doesn't support subdomains

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,21 +6,14 @@ export const config = {
   matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
 }
 
-function getDocsProject(host: string) {
-  const parts = host.split(".")
-
-  if (
-    (host.endsWith(`localhost:${process.env.PORT}`) && parts.length === 2) ||
-    (host.endsWith(".vercel.app") && parts.length === 4) ||
-    (host.endsWith("kevinwolf.cr") && parts.length === 3)
-  ) {
-    return parts[0]
-  }
-
-  return null
-}
-
 export function middleware(req: NextRequest) {
-  const docsProject = getDocsProject(req.headers.get("host")!)
+  const baseUrl = "kevinwolf.cr"
+
+  const docsProject =
+    req.headers
+      .get("host")!
+      .replace(`localhost:${process.env.PORT}`, baseUrl)
+      .replace(new RegExp(`^(?:([^.]+)\\.)?${baseUrl.replace(/\./g, "\\.")}$`), "$1") || ""
+
   if (docsProject) return NextResponse.rewrite(new URL(`/docs/${docsProject}${req.nextUrl.pathname}`, req.url))
 }

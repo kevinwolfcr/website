3ddbab56e295870156cb87a8b0a288c878e51d85
Kevin Wolf <hi@kevinwolf.cr>
2023-08-31 11:16:53 -0600

    perf: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+// eslint-disable-next-line import/no-unresolved
+import bundleAnalizer from "@next/bundle-analyzer"
+
+const withBundleAnalyzer = bundleAnalizer({
+  // eslint-disable-next-line no-undef
+  enabled: process.env.ANALYZE === true,
+  openAnalyzer: false,
+})
+
 /** @type {import('next').NextConfig} */
-export default {
+export default withBundleAnalyzer({
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: { remotePatterns: [{ hostname: "**" }] },
   experimental: { serverActions: true },
-}
+})

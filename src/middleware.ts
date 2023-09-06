1cb9363294f30e5d2314c523e8774bf3402a9d6a
Kevin Wolf <hi@kevinwolf.cr>
2023-09-05 18:41:56 -0600

    fix: allow deploy preview urls in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,16 @@ import type { NextRequest } from "next/server"
 
 import { NextResponse } from "next/server"
 
+import { siteUrl } from "./utils/seo"
+
 export const config = {
   matcher: ["/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)"],
 }
 
 export function middleware(req: NextRequest) {
+  const currentUrl = siteUrl().replace(/^https?:\/\//, "")
   const baseUrl = "kevinwolf.cr"
-
-  const docsProject =
-    req.headers
-      .get("host")!
-      .replace(`localhost:${process.env.PORT}`, baseUrl)
-      .replace(new RegExp(`^(?:([^.]+)\\.)?${baseUrl.replace(/\./g, "\\.")}$`), "$1") || ""
-
+  const url = req.headers.get("host")!.replace(currentUrl, baseUrl)
+  const docsProject = url.replace(new RegExp(`^(?:([^.]+)\\.)?${baseUrl.replace(/\./g, "\\.")}$`), "$1") || ""
   if (docsProject) return NextResponse.rewrite(new URL(`/docs/${docsProject}${req.nextUrl.pathname}`, req.url))
 }
